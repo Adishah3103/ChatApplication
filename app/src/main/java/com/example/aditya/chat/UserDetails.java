c29d75ca2d9be9e594b2abf4cb95498b78a6677f
Adishah3103 <adishah3103@gmail.com>
2018-03-25 22:31:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/chat/UserDetails.java
@@ -0,0 +1,12 @@
+package com.example.aditya.chat;
+
+/**
+ * Created by Aditya on 25-03-2018.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+
+}
